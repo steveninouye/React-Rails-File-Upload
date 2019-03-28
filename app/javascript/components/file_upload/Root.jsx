d2a7ce71a29d5c288869168f7cc269a1d5ad2898
Steven Inouye <steveninouye@msn.com>
2019-03-28 08:02:07 -0700

    Configure #handleChange on Root component to instantiate new FormData object and append document and document type to created object

--- app/javascript/components/file_upload/Root.jsx
+++ app/javascript/components/file_upload/Root.jsx
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import PassingUpload from './components/PassingUpload';
 
 class Root extends Component {
-  handleChange(type) {}
+  handleChange(type) {
+    returns((e) => {
+      const file = e.target.files[0];
+      const formData = new FormData();
+      formData.append('document', file);
+      formData.append('document_type', type);
+    });
+  }
 
   render() {
     const { handleChange } = this;
-    
+
     return (
       <React.Fragment>
         Root Component
--- app/javascript/components/file_upload/components/PassingUpload.jsx
+++ app/javascript/components/file_upload/components/PassingUpload.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const PassingUpload = (props) => (
+const PassingUpload = ({ uploadFile }) => (
   <React.Fragment>
     <input
       type="file"
       accept="image/*,application/pdf"
-      onChange={this.uploadFile('passing')}
+      onChange={uploadFile('passing')}
       // multiple           multiple keyword can be used to allow user to upload multiple files
     />
   </React.Fragment>
