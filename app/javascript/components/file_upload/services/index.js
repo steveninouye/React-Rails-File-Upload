1db853cc62f9b3783c09cb5d294855f8a7a1dcfb
Steven Inouye <steveninouye@msn.com>
2019-03-28 11:21:12 -0700

    Create service for GET

--- app/javascript/components/file_upload/services/index.js
+++ app/javascript/components/file_upload/services/index.js
@@ -1,13 +1,15 @@
 export const authToken = document.querySelector('meta[name="csrf-token"]')
   .content;
+export const authTokenHeader = { 'X-CSRF-Token': authToken };
 
-export const get = (url) => fetch(url).then((res) => res.json());
+export const get = (url) =>
+  fetch(url, { method: 'GET', headers: { ...authTokenHeader } }).then((res) =>
+    res.json()
+  );
+
 export const postFile = (url, fileData) =>
   fetch(url, {
     method: 'POST',
     body: fileData,
-    headers: { 'X-CSRF-Token': authToken }
+    headers: { ...authTokenHeader }
   }).then((res) => res.json());
-
-// .setRequestHeader('X-CSRF-Token', token)
-// "?&authenticity_token="
