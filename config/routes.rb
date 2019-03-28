93c1f0a5964d8504ae766df5e9c9e8bf0b1d4d3f
Steven Inouye <steveninouye@msn.com>
2019-03-27 23:34:28 -0700

    Configure purchase orders routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'purchase_orders/index'
-  get 'purchase_orders/show'
-  get 'purchase_orders/new'
-  get 'purchase_orders/create'
-  get 'purchase_orders/edit'
-  get 'purchase_orders/update'
-  get 'purchase_orders/destroy'
+  resources :purchase_orders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
